1067474d98412e5bb8673885f307e112e1a8374f
BatanAndrei <yabatan777@gmail.com>
2023-11-30 12:49:36 +0300

    initial commit

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,7 +4,10 @@ export default function Profile() {
     return (
         <div className={styles.containerProfile}>
             <div className={styles.infoProfile}></div>
-            <div className={styles.aboutProfile}></div>
+            <div className={styles.aboutProfile}>
+                <h2 className={styles.aboutProfileTitle}>ABOUT ME</h2>
+                <h3 className={styles.aboutProfileText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in ante consequat, ornare nisi et, ultrices libero. Nunc nibh dolor, maximus quis auctor nec, tempor quis ipsum. Proin mollis pellentesque nulla ac varius.</h3>
+            </div>
         </div>
     )
 }
