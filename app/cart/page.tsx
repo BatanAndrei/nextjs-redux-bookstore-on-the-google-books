0359f3355f180346bbff191ce61dbd04738a035c
BatanAndrei <yabatan777@gmail.com>
2023-11-30 16:19:12 +0300

    initial commit

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,15 @@
+import styles from './cart.module.css'
+
 export default function Cart() {
     return (
-        <h1 className="global">Cart</h1>
+        <div className={styles.containerCart}>
+            <h2 className={styles.titleCart}>SHOPPING CART</h2>
+            <div className={styles.columnBlock}>
+                <h3 className={styles.nameColumn}>ITEM</h3>
+                <h3 className={styles.nameColumn}>QUANTITY</h3>
+                <h3 className={styles.nameColumn}>PRICE</h3>
+                <h3 className={styles.nameColumn}>DELIVERY</h3>
+            </div>
+        </div>
     )
 }
