856fe1ae7c32742df2eae5a1a3a10c96bf2524a8
BatanAndrei <yabatan777@gmail.com>
2023-12-01 16:44:57 +0300

    initial commit

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -8,9 +8,9 @@ export default function LoginForm() {
             <h2 className={montserrat.className+' '+styles.titleLogin}>Log in</h2>
             <form className={styles.form}>
                 <h2 className={montserrat.className+' '+styles.titleInput}>Email</h2>
-                <input className={styles.input} type="text" name="name" />
+                <input className={styles.input} type="email" name="email" required placeholder="Enter your email..." />
                 <h2 className={montserrat.className+' '+styles.titleInput}>Password</h2>
-                <input className={styles.input} type="text" name="name" />
+                <input className={styles.input} type="password" name="login" required placeholder="Enter your password..." />
                 <p className={montserrat.className+' '+styles.warn} >Your password must be at least 6 characters long</p>
                 <button className={styles.button} type="submit">LOG IN</button>
             </form>
